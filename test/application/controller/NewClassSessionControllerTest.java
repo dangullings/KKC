1237892f3a0aea90b6fa855122b008f72e5e0208
Unknown <dangullings@gmail.com>
2018-10-07 10:55:29 -0500

    button image update

---
+++ test/application/controller/NewClassSessionControllerTest.java
@@ -0,0 +1,17 @@
+package application.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NewClassSessionControllerTest {
+
+    private NewClassSessionController newClassSessionController = new NewClassSessionController();
+
+    @Test
+    public void Test1 (){
+
+
+    }
+
+}
